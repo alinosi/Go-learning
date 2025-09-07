f20a52066a82481505042191116533f8a9df90b3
alinosi <mthoriqulfadli@gmail.com>
2025-09-07 22:01:39 +0700

    update OnlyIn function to use consistent test data and add TestBufferedChannel for buffered channel behavior

--- goroutines/channel_test.go
+++ goroutines/channel_test.go
@@ -41,7 +41,7 @@ func TestChannelAsParameter(t *testing.T) {
 
 func OnlyIn(channel chan<- string) {
 	time.Sleep(2 * time.Second)
-	channel <- "Eko Kurniawan Khannedy"
+	channel <- "Sir Noby BItzer"
 }
 
 func OnlyOut(channel <-chan string) {
@@ -57,4 +57,24 @@ func TestInOutChannel(t *testing.T) {
 	go OnlyOut(channel)
 
 	time.Sleep(5 * time.Second)
+}
+
+func TestBufferedChannel(t *testing.T) {
+	channel := make(chan string, 3)
+	defer close(channel)
+
+	go func() {
+		channel <- "Sir"
+		channel <- "Noby"
+		channel <- "BItzer"
+	}()
+
+	go func() {
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+	}()
+
+	time.Sleep(2 * time.Second)
+	fmt.Println("Selesai")
 }
