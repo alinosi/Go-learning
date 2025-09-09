2095f79efc0e0df3d3bd724552fa0e93742cddf3
alinosi <mthoriqulfadli@gmail.com>
2025-09-09 08:13:17 +0700

    jkkj

---
+++ tes.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string strx = to_string(x);
+        if (strx.front() == strx.back()) {
+            if ( strx.size()-2 % 2 == 0) {
+                return true;
+            } else if ()
+                
+        }
+    }
+};
