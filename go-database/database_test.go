86b9a4307c6e51f4f73c90ff5f892896e0f681a4
alinosi <mthoriqulfadli@gmail.com>
2025-10-20 09:42:52 +0700

    docs: add comments to explain database/sql package interface and usage and why it's called contract/interface

--- go-database/database_test.go
+++ go-database/database_test.go
@@ -11,6 +11,19 @@ func TestEmpty(t *testing.T) {
 
 }
 
+/**
+	package database/sql merupakan seuah interface yang
+	mendefinisakn kontrak yang harus dimiliki driver agar bisa digunakan untuk berinteraksi dengan dbms
+**/
+
+// dengan adanya package database maka kita bisa menggunakan func yang sama untuk menjalankan database apapun
+
+// karena prinsip dari interface sendiri adalah polimorfism
+
+// yang berbeda hanyalah nama driver dan kode sqlnya
+
+// buktinya kita menggunakan metode dari package sql seperti sql.Open dll
+
 func TestOpenConnection(t *testing.T) {
 	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/belajar_golang_database")
 	if err != nil {
