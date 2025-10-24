8ada86ab0f8259988575f3e31ba5db6b79ba4c44
alinosi <mthoriqulfadli@gmail.com>
2025-10-24 14:27:18 +0700

    feat: add TestExecSqlParameter to demonstrate parameterized SQL execution and prevent SQL injection

--- go-database/sql_test.go
+++ go-database/sql_test.go
@@ -122,3 +122,21 @@ func TestSqlInjection(t *testing.T) {
 		fmt.Println("Gagal Login")
 	}
 }
+
+func TestExecSqlParameter(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	username := "noby'; DROP TABLE user; #"
+	password := "sasuke12"
+
+	script := "INSERT INTO user(username, password) VALUES(?, ?)"
+	_, err := db.ExecContext(ctx, script, username, password)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Success insert new user")
+}
