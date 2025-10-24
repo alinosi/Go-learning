f5938bb7a75e80e83ca923bb5b871d41cd9423db
alinosi <mthoriqulfadli@gmail.com>
2025-10-24 14:29:14 +0700

    feat: add TestSqlInjectionSafe to demonstrate safe SQL execution using parameterized queries

--- go-database/sql_test.go
+++ go-database/sql_test.go
@@ -123,6 +123,36 @@ func TestSqlInjection(t *testing.T) {
 	}
 }
 
+func TestSqlInjectionSafe(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	username := "admin'; #"
+	password := "salah"
+
+	// user parameter instead of concatenation
+	script := "SELECT username FROM user WHERE username = ? AND password = ? LIMIT 1"
+	fmt.Println(script)
+	rows, err := db.QueryContext(ctx, script, username, password) // variable position must be sequential
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	if rows.Next() {
+		var username string
+		err := rows.Scan(&username)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Sukses Login", username)
+	} else {
+		fmt.Println("Gagal Login")
+	}
+}
+
 func TestExecSqlParameter(t *testing.T) {
 	db := GetConnection()
 	defer db.Close()
